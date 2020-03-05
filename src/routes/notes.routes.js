867a795d0bab873e5dcf8561aa88ce06dee1cca7
Victor Bejarano <alejandrobjrn4@gmail.com>
2020-03-04 22:08:33 -0500

    Create CRUD controllers

---
+++ src/routes/notes.routes.js
@@ -0,0 +1,31 @@
+const { Router } = require('express');
+const router = Router();
+
+const { 
+    renderNoteForm, 
+    createNewNote, 
+    renderNotes, 
+    renderEditForm, 
+    updateNote, 
+    deleteNote 
+} = require('../controllers/notes.controller');
+
+//CRUD
+//New note
+router.get('/notes/add', renderNoteForm);
+
+router.post('/notes/add', createNewNote);
+
+//Get All Note
+
+router.get('/notes', renderNotes);
+
+//Edit Notes
+router.get('/notes/edit/:id', renderEditForm);
+
+router.put('/notes/edit/:id', updateNote);
+
+// Delete Notes
+router.delete('/notes/delete/:id', deleteNote);
+
+module.exports = router;
