a528c1a53700baf95459399731f61ee08a92cfe2
Victor Bejarano <alejandrobjrn4@gmail.com>
2020-03-04 19:19:52 -0500

    Implements Bootstrap 4

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');//modulo de direcciones
 
 //Inicializaciones
@@ -8,7 +9,13 @@ const app = express();
 
 app.set('port', process.env.PORT || 4000);
 app.set('views', path.join( __dirname,'views'));//establece la ruta de view sin importar SO
-
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs')
 //Middlewares funciones que se 
 //ejecutan a medida que llegan peticiones
 
@@ -19,7 +26,7 @@ app.use(express.urlencoded({extended: false}));
 //Routes
 
 app.get('/', (req, res) =>{
-    res.send('hello world');
+    res.render('index');
 });
 
 //Archivos Estaticos
